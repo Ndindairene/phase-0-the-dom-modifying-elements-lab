076173ee9ef20617b62ff67fc4243101191eeab2
Ndindairene <irene.ndindaa@gmail.com>
2023-10-24 22:00:17 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 // Write your code here!
-main.remove();
-const newElement = document.createElement('h1');
-newElement.id = 'victory';
-document.body.appendChild(newElement);
-const newHeader = document.querySelector('h1#victory');
+// main.remove();
+
+// const newElement = document.createElement('h1');
+
+// newElement.id = 'victory';
+// document.body.appendChild(newElement);
+// const newHeader = document.querySelector('h1#victory');
+// newHeader.innerHTML = "YOUR-NAME is the champion";
+
+document.querySelector('main#main').remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.id = 'victory';
 newHeader.innerHTML = "YOUR-NAME is the champion";
 
+document.body.appendChild(newHeader);
